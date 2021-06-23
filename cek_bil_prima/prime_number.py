adf1df0f6e0b4255a6c4c9964598cf24dbb1c4b1
GitTest <holilhaqalim@gmail.com>
2021-06-23 22:51:41 +0800

    update code

--- cek_bil_prima/prime_number.py
+++ cek_bil_prima/prime_number.py
@@ -6,20 +6,18 @@ input_user = int(input("Masukkan angka: "))
 if input_user == 1:
     print(input_user, "bukan bilangan prima.")
 else:
-    arr = []
+    count = 0
     if input_user % 1 == 0:
         for i in range(1, input_user+1):
             if input_user % i != 0:
                 continue
             else:
-                arr.append(i)
+                count += 1
     else:
         print(input_user)
         
-    if len(arr) > 2:
+    if count > 2:
         print(input_user, "bukan bilangan prima.")
     else:
         print(input_user, "adalah bilangan prima.")
-    
-    print(arr)
 
